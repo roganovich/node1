eaa3add7b17587a1ef769188b354f90dc488ca90
Roman Roganovich (Soft) <roman.roganovich@mail.ru>
2020-12-22 11:57:28 +0300

    funciton

--- function.js
+++ function.js
@@ -2,4 +2,14 @@ function test(){
   console.log("function test");
 }
 
+function call(funcName){
+  funcName()
+}
+
+var stringFunction = function(){
+  console.log("function stringFunction");
+}
+
+
 test();
+call(stringFunction)
